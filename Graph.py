ce6881562e2e4b7e1f67666c899b74dd6d8ed8b1
Brandon Kamaka <Bkamaka@gmail.com>
2018-08-20 20:12:46 -0400

    Implemented basic graph functionality.

--- Graph.py
+++ Graph.py
@@ -1,4 +1,9 @@
+import random
+from math import sqrt
+
 import networkx as nx
+import matplotlib.pyplot as plot
+
 
 # size = 15
 #
@@ -23,9 +28,48 @@ import networkx as nx
 # plot.show()
 # print(g.adj)
 
-file = open('web-Google.txt', 'rb')
-g = nx.read_adjlist(file)
-print(g.adj)
-print(nx.info(g))
 
-giter = nx.nodes
+def get_google_graph():
+    file = open('web-Google.txt', 'rb')
+    g = nx.read_adjlist(file)
+
+    # we add weights just to make the graph more topologically interesting for project purposes
+    random.seed('seed')
+    for (u, v, w) in g.edges(data=True):
+        w['weight'] = random.randint(0, 10)
+
+    return g
+
+
+def get_random_graph(size, seed=None):
+    """wrapper for NetworkX ERdos-Renyi random graph"""
+    edge_probability = .1
+    g = nx.fast_gnp_random_graph(size, edge_probability, seed)
+    return g
+
+
+def draw(graph: nx.Graph, solution_nodes=()):
+    optimal_spacing = 3 / sqrt(graph.number_of_nodes())
+
+    # default types must be immutable, but we want a list to work on
+    solution_nodes = list(solution_nodes)
+    solution_edges = [(solution_nodes[i], solution_nodes[i + 1]) for i in range(len(solution_nodes) - 1)]
+
+    # first we determine node spacing using a standard force model
+    pos = nx.spring_layout(graph, k=optimal_spacing)
+
+    # we draw the whole graph with the default color
+    # then we redraw the path using different attributes
+    # this avoids having to partition the graph to draw each only once
+    nx.draw_networkx_nodes(graph, pos, node_color='r', node_size=100)
+    nx.draw_networkx_edges(graph, pos, edge_color='r', width=1, )
+
+    nx.draw_networkx_nodes(graph, pos, nodelist=solution_nodes, node_color='b', node_size=500)
+    nx.draw_networkx_edges(graph, pos, edgelist=solution_edges, edge_color='b', width=2)
+
+    plot.axis('off')
+    plot.show()
+
+if __name__ == '__main__':
+    g = get_random_graph(30, 'test')
+    draw(g)
